b08c840bfbd6cc01bdc75e77cd68911810ad6082
mdamaan00 <mdamaan00@4b5d82362e5e>
2021-07-03 17:36:32 +0000

    version 1.0.7

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "ritextiles0@gmail.com"
-EMAIL_HOST_PASSWORD = "textile123456789"
+EMAIL_HOST_PASSWORD = "Goku@7977"
